edda216caad970b308f8771a41c3442a402aea30
rahillakhani <lakhani.rj@gmail.com>
2015-11-24 11:15:32 +0530

    Footer text change sub service link text

--- v2/lib/common.js
+++ v2/lib/common.js
@@ -18,6 +18,23 @@ $(document).ready(function() {
 
 	$(".show-drop-nav").bind("mouseenter mouseleave",function(){
 		
-		$(this).find(".drop-nav").toggle();
+		$(this).find(".drop-nav").toggle(
+			/*function(){
+				$(".drop-nav a").bind("mouseenter",
+					function(){
+						console.log("state");
+						$(".show-drop-nav > a").css({
+							"background":"#088CC0",
+							"color":"#ffffff"
+						});
+					}).bind("mouseleave",
+					function(){
+						console.log("state");
+						$(".show-drop-nav > a").css({
+							"background":"#fff",
+							"color":"#088CC0"
+						});
+					});
+				}*/);		
 	});
 });
